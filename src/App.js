e17ad25e0449b80600346f6aac2121ee871848b3
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-02-23 10:53:12 +0900

    marker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
-import React from "react";
-import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
-
+import React,{useState} from "react";
+import axios from 'axios';
+import jsonFile from "./example.json";
+import ngeohash from 'ngeohash'
+import { RenderAfterNavermapsLoaded, NaverMap, Marker,Rectangle } from "react-naver-maps";
+// client id = 6qwkueng4z
+  //MskYLQOWYg7kMTlwFD4oVJcmshWhhu5ehGfNq9s0
+ // const NAVER_API_KEY = '6qwkueng4z';
+let markArr=[];
 function NaverMapAPI() {
+  
+  const navermaps = window.naver.maps;
+  const min = {x: 126.5322317, y: 33.3572421};
+  const max = {x: 126.5364907, y: 33.3608829};
+  const bounds = {
+    north: max.y,
+    east: max.x,
+    south: min.y,
+    west: min.x
+  };
+  
   return (
     <div>
       <NaverMap
@@ -14,20 +31,72 @@ function NaverMapAPI() {
       defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
       defaultZoom={13} // 지도 초기 확대 배율
       
-    />
+    >
+      {markArr.map((data,i)=>{
+        return(
+            <Marker
+              key={i}
+              position={new navermaps.LatLng(data.latitude, data.longitude)}
+              animation={2}
+              onClick={() => {alert("여기는 ~ 입니다.");}}
+            />
+        )
+      })}
+
+    {/* <Rectangle 
+        bounds={bounds}
+        fillOpacity={0.5}
+        fillColor={'#FF0000'}
+        strokeColor={'red'}
+        clickable={true} // click event를 다루기 위해서는 true로 설정되어야함.
+        onClick={() => {
+          alert('여기는 한라산 입니다.')
+        }}
+      /> */}
+    </NaverMap>
+    
     </div>
     
     
   );
 }
 
-function App() {
+const  App=()=> {
+  const [jsonList,setJsonList] = useState([]);
+  let arr=[];
+  jsonFile.map((data,i)=>{
+    arr.push(ngeohash.decode(data.geohash))
+    //console.log(ngeohash.decode(data.geohash))
+  })
+  markArr=arr;
+  console.log(markArr)
+  // console.log(jsonFile.map((data,i)=>{
+  //   return(
+  //     data
+  //   )
+  // }))
+  
+
+  // var markerClustering = new MarkerClustering({
+  //   minClusterSize: 2,
+  //   maxZoom: 8,
+  //   map: map,
+  //   markers: markers,
+  //   disableClickZoom: false,
+  //   gridSize: 120,
+  //   indexGenerator: [10, 100, 200, 500, 1000],
+  //   stylingFunction: function(clusterMarker, count) {
+  //       $(clusterMarker.getElement()).find('map').text(count);
+  //   }
+  // });
+  
   return (
     <RenderAfterNavermapsLoaded
-      ncpClientId={'apikey'} // 자신의 네이버 계정에서 발급받은 Client ID
+      ncpClientId={'6qwkueng4z'} // 자신의 네이버 계정에서 발급받은 Client ID
       error={<p>Maps Load Error</p>}
       loading={<p>Maps Loading...</p>}
     >
+    
       <NaverMapAPI />
     </RenderAfterNavermapsLoaded>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div id="map" ></div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
